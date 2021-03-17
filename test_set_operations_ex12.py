90de754b21f53f2582deb0c6fd37420ff2223fc9
Kuldar Vakker <kuvakk@ttu.ee>
2021-03-17 19:03:56 +0200

    just a copy.

--- test_set_operations_ex12.py
+++ test_set_operations_ex12.py
@@ -5,7 +5,23 @@ import pytest
 from operators.leaf import Leaf
 from operators.add import Add
 from operators.mul import Mul
+from operators.div import Div
+from operators.sub import Sub
+from operators.or_ import Or
+from operators.pow import Pow
+from operators.xor import Xor
 
+
+@pytest.mark.timeout(1.0)
+def test_xor_bitwise_xor_when_given_logical_xor():
+    """."""
+    assert Xor(Xor(Leaf(500), Leaf(5)), Xor(Leaf(20), Leaf(4))).apply() == 481
+
+
+@pytest.mark.timeout(1.0)
+def test_pow_raises_to_power_when_given_exponents():
+    """."""
+    assert Pow(Pow(Leaf(3), Leaf(2)), Pow(Leaf(2), Leaf(4))).apply() == 1853020188851841
 
 @pytest.mark.timeout(1.0)
 def test_set_leaf_apply_yields_set_when_given_a_set():
@@ -17,6 +33,7 @@ def test_set_leaf_apply_yields_set_when_given_a_set():
 def test_set_add_adds_when_given_leaves_with_sets():
     """."""
     assert Add(Leaf({5}), Leaf({6})).apply() == {5, 6}
+    assert Add(Leaf({5}), Leaf(6)).apply() == {5, 6}
 
 
 @pytest.mark.timeout(1.0)
@@ -30,3 +47,27 @@ def test_set_multiply_multiplies_when_given_leaves_with_sets__second_multiple_el
     """."""
     assert Mul(Leaf({5, 6}), Leaf({3, 4})).apply() == {frozenset({5, 3}), frozenset({6, 3}), frozenset({5, 4}),
                                                        frozenset({6, 4})}
+
+
+@pytest.mark.timeout(1.0)
+def test_set_div_divides_when_given_leaves_with_sets():
+    """."""
+    assert Div(Leaf({5, 6}), Leaf({6})).apply() == {5}
+
+
+@pytest.mark.timeout(1.0)
+def test_set_division_subtracts_when_given_subtraction_with_int():
+    """."""
+    assert Div(Sub(Leaf({5}), Leaf({7})), Div(Leaf(5), Leaf(6))).apply() == {5}
+
+
+@pytest.mark.timeout(1.0)
+def test_set_subtract_subtracts_when_given_subtraction_with_int():
+    """."""
+    assert Sub(Sub(Leaf({5}), Leaf({7})), Sub(Leaf(5), Leaf(6))).apply() == {5}
+
+
+@pytest.mark.timeout(1.0)
+def test_set_multiply_multiplies_when_given_leaves_with_sets__second_number():
+    """."""
+    assert Mul(Leaf({5, 6}), Leaf(4)).apply() == {frozenset({5, 4}), frozenset({6, 4})}
--- tree_node.py
+++ tree_node.py
@@ -1,5 +1,4 @@
 """."""
-
 from abc import ABCMeta, abstractmethod
 
 
@@ -8,6 +7,14 @@ class TreeNode(metaclass=ABCMeta):
 
     def __init__(self, *args):
         """:param make use of *args and store them in a way that it is easy to use them."""
+        self.__value = args  # Tuple of values
+        pass
+
+    @property
+    @abstractmethod
+    def default_operator(self):
+        """abstract method which should be overridden to return the default_operator object."""
+        #  return lambda *x: x
         pass
 
     @abstractmethod
@@ -15,20 +22,48 @@ class TreeNode(metaclass=ABCMeta):
         """abstract method which should be overridden to compute the value of the given abstract tree."""
         pass
 
-    @abstractmethod
     def class_str(self):
         """:return class string representation of the object."""
-        pass
-
-    @abstractmethod
-    def __str__(self):
-        """:return string representation of the object."""
-        pass
+        return f"{self.__name__}({', '.join([x.class_str() for x in self.__value])})"
 
     def __eq__(self, other):
         """:return True when 2 object trees have the same shape and values."""
+        params = [x for x in self.__value]
+        types = tuple(type(x) for x in params)
+        if types == other:
+            return True
         return False
 
     def __ne__(self, other):
         """:return True when 2 object trees have a different shape and/or values."""
+        params = [x for x in self.__value]
+        types = (type(x) for x in params)
+        if types != other:
+            return True
         return False
+
+    def __encase(self, node):
+        """."""
+        if node.priority < self.priority or node.priority == self.priority and node.associativity is True:
+            return str(node)
+        return str(f"({node})")
+
+    def __str__(self):
+        """."""
+        return f" {self.default_operator} ".join([self.__encase(x) for x in self.__value])
+
+    @property
+    @abstractmethod
+    def priority(self):
+        """
+        abstract method witch should be overridden to return priority of the node.
+
+        Visit: https://en.wikipedia.org/wiki/Order_of_operations
+        """
+        pass
+
+    @property
+    @abstractmethod
+    def associativity(self):
+        """abstract method witch should be overridden to return a boolean whether the node is associative or not."""
+        pass
